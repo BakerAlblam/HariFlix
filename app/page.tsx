edd51c52ee221c23c21637fc48462286bcef7895
BakerAlblam <gallardo0@outlook.com>
2024-01-21 19:43:22 +0100

    Loading

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,19 @@ import TrendingCarousel from '@/components/TrendingMovies';
 import TrendingTv from '@/components/TrendingTv';
 import TrendingAll from '@/components/TrendingAll';
 import TrendingPeople from '@/components/TrendingPeople';
+import { Suspense } from 'react';
+import Loading from '@/components/Loading';
 
 export default async function Page() {
   return (
-    <main className="sm:p-14 py-16 px-3 flex flex-col gap-2 bg-[#0F1117]">
-      <h1 className="text-3xl text-white text-center font-bold">Trending</h1>
-      <TrendingAll />
-      <TrendingCarousel />
-      <TrendingTv />
-      <TrendingPeople />
-    </main>
+    <Suspense fallback={<Loading />}>
+      <main className="sm:p-14 py-16 px-3 flex flex-col gap-2 bg-[#0F1117]">
+        <h1 className="text-3xl text-white text-center font-bold">Trending</h1>
+        <TrendingAll />
+        <TrendingCarousel />
+        <TrendingTv />
+        <TrendingPeople />
+      </main>
+    </Suspense>
   );
 }
