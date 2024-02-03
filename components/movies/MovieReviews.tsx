b349ae51adea4689db7b8871b9b8ede30f8f6145
BakerAlblam <gallardo0@outlook.com>
2024-02-03 12:40:50 +0100

    revie aqorddion

--- components/movies/MovieReviews.tsx
+++ components/movies/MovieReviews.tsx
@@ -3,6 +3,12 @@ import axios from 'axios';
 import { Card } from '../ui/card';
 import { Avatar } from '@nextui-org/react';
 import { Star } from 'lucide-react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 type Data = {
   id: number;
@@ -28,36 +34,49 @@ export default async function MovieReviews({ data }: { data: any }) {
       <div className="space-y-4">
         <h2 className="text-2xl font-bold">
           Ratings & Reviews -{' '}
-          <span className="p-2 bg-slate-600 rounded-lg">{rev?.length}</span>
+          <span className="p-2 bg-slate-600 rounded-full">{rev?.length}</span>
         </h2>
         {rev?.map((r: Data) => (
-          <Card key={r?.id}>
-            <div className="flex items-center mb-4 ml-1 mt-1">
-              <Avatar
-                src={`https://image.tmdb.org/t/p/original${r?.author_details?.avatar_path}`}
-                size="md"
-                fallback={'NA'}
-              />
-              <div className="font-medium dark:text-white ml-1 flex flex-col">
-                <p className="mt-1">{r?.author}</p>
-                <div className="flex items-center">
-                  <Star
-                    size={20}
-                    color="gold"
-                  />
-                  <span className="ml-1 mb-0.5 text-rose-700">
-                    {' '}
-                    {r.author_details?.rating}{' '}
-                  </span>
-                </div>
-              </div>
-            </div>
+          <Accordion
+            type="single"
+            collapsible
+            key={rev?.id}
+          >
+            <AccordionItem value={`r?.id`}>
+              <AccordionTrigger>
+                {r?.author} - {r?.created_at.toString().substring(0, 10)}
+              </AccordionTrigger>
+              <AccordionContent key={r?.id}>
+                <Card>
+                  <div className="flex items-center mb-4 ml-1 mt-1">
+                    <Avatar
+                      src={`https://image.tmdb.org/t/p/original${r?.author_details?.avatar_path}`}
+                      size="md"
+                      fallback={'NA'}
+                    />
+                    <div className="font-medium dark:text-white ml-1 flex flex-col">
+                      <p className="mt-1">{r?.author}</p>
+                      <div className="flex items-center">
+                        <Star
+                          size={20}
+                          color="gold"
+                        />
+                        <span className="ml-1 mb-0.5 text-rose-700">
+                          {' '}
+                          {r.author_details?.rating}{' '}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
 
-            <p className="mb-2 text-gray-800 mx-1.5">{r?.content}</p>
-            <p className="mb-2 text-gray-800 mx-1.5 text-sm font-semibold">
-              {r?.created_at.toString().substring(0, 10)}
-            </p>
-          </Card>
+                  <p className="mb-2 text-gray-800 mx-1.5">{r?.content}</p>
+                  <p className="mb-2 text-gray-800 mx-1.5 text-sm font-semibold">
+                    {r?.created_at.toString().substring(0, 10)}
+                  </p>
+                </Card>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         ))}
       </div>
     </div>
