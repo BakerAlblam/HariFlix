47583bc843c0798a26478134312069713c6227e1
BakerAlblam <gallardo0@outlook.com>
2024-02-06 19:50:36 +0100

    foot

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-4">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link
             href="/"
